a71fc78d3b4b3baf0ee3ade0e9eb57296dc48188
jcromerohdz <jcromerohdz@gmail.com>
2024-04-14 12:18:03 -0700

    Add task functionality to the todo project

--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Task
 
-# Register your models here.
-admin.site.register(Task)
+# Register your models here
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task', 'is_completed', 'update_at')
+
+admin.site.register(Task, TaskAdmin)
--- todo_main/views.py
+++ todo_main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from todo.models import Task
 
 def home(request):
-    tasks = Task.objects.filter(is_completed=False)
+    tasks = Task.objects.filter(is_completed=False).order_by('-update_at')
     context = {
         'tasks': tasks
     }
