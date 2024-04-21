a18bc75569505fe5536e10211806546ade4a160e
jcromerohdz <jcromerohdz@gmail.com>
2024-04-21 08:42:57 -0700

    The app now is eble to edit tasks

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
     path('task_done/<int:id>/', views.task_done, name='task_done'),
+    path('task_undone/<int:id>/', views.task_undone, name='task_undone'),
+    path('edit_task/<int:id>/', views.edit_task, name='edit_task'),
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -17,3 +17,22 @@ def task_done(request, id):
 
     return redirect('home')
     
+def task_undone(request, id):
+    task = get_object_or_404(Task, id=id)
+    task.is_completed = False
+    task.save()
+
+    return redirect('home')
+
+def edit_task(request, id):
+    get_task = get_object_or_404(Task, id=id)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task,
+        }
+        return render(request, 'edit_task.html', context)
